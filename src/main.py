fc398fa5865ba983be2e4903d121135e80e46b0e
Tamicktom <rickpokemon38@gmail.com>
2026-01-10 18:46:11 -0300

    basic route

--- src/main.py
+++ src/main.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, request
+from models.task import Task, CreateTaskSchema
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, World!"
+tasks = []
+
+@app.get("/task")
+def index_tasks():
+    return tasks
+
+@app.post("/task")
+def store_task():
+    data = request.get_json()
+
+    parsedData = CreateTaskSchema.model_validate(data)
+
+    id = tasks.__len__() + 1
+
+    task = Task(
+            id,
+            parsedData.title,
+            parsedData.description,
+            parsedData.completed
+            )
+
+    tasks.append(task)
+
+    print(tasks)
+
+    return "OK"
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ src/models/task.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, ValidationError, Field
+
+class CreateTaskSchema(BaseModel):
+    title: str
+    description: str
+    completed: bool
+
+class Task:
+    def __init__(self, id, title, description, completed = False) -> None:
+        self.id = id
+        self.title = title
+        self.description = description
+        self.completed = completed
+
+    def to_dict(self):
+        return {
+                "id": self.id,
+                "title": self.title,
+                "description": self.description,
+                "completed" : self.completed
+                }
+
+if __name__ == "__main__":
+    pass
