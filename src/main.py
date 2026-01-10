cabbdec2923aa93d7129b1ccbba3a7a835946bf0
Tamicktom <rickpokemon38@gmail.com>
2026-01-10 12:49:44 -0300

    Update dependencies in pyproject.toml, enhance pdm.lock with new packages, and refactor main.py to set up a basic Flask application.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+from flask import Flask
 
-def main():
-    print("Hello, World!")
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
